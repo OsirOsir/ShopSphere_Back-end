acf65a46534245f462e797bbc9dea57f02babbbc
CalebKiK <karimicaleb@gmail.com>
2024-10-15 19:57:33 +0300

    Database seeded

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify, request
-from models import db, User, Item
+from models import db, User, Item, SpecialCategory
 from flask_migrate import Migrate
-from serializers import (user_serializer, clothes_serializer, whatsnew_serializer, 
-                         flashsale_serializer, hotincategory_serializer, artwork_serializer, 
-                         shoes_serializer, electronics_serializer, book_serializer, 
-                         cart_serializer, cartitem_serializer)
+from serializers import (user_serializer, item_serializer)
 
 app = Flask(__name__)
 
@@ -43,66 +40,94 @@ def handle_user(user_id):
         db.session.delete(user)
         db.session.commit()
         return {"message": f"User with id {user_id} has been deleted"}
+    
+    
+@app.route("/api/item/<int:item_id>/add_special_category", methods=["POST"])
+def add_special_category_to_item(item_id):
+    data = request.json
+    special_category_name = data["special_category_name"]
+    item = Item.query.get(item_id)
+    special_category = SpecialCategory.query.filter_by(name=special_category_name).first()
 
-# Clothes routes
-@app.route('/clothes', methods=['GET'])
-def handle_clothes():
-    clothes = Clothes.query.all()
-    return jsonify([clothes_serializer(cloth) for cloth in clothes])
+    if special_category and item:
+        item.special_categories.append(special_category)
+        db.session.commit()
+        return jsonify({"message": f"Special Category {special_category_name} added to item"}), 200
+    return jsonify({"message": "Error: Item or Special Category not found"}), 404
 
-# Whats New routes
-@app.route('/whats_new', methods=['GET'])
-def handle_whats_new():
-    whats_new_items = WhatsNew.query.all()
-    return jsonify([whatsnew_serializer(item) for item in whats_new_items])
+@app.route("/api/item/<int:item_id>/remove_special_category", methods=["POST"])
+def remove_special_category_from_item(item_id):
+    data = request.json
+    special_category_name = data["special_category_name"]
+    item = Item.query.get(item_id)
+    special_category = SpecialCategory.query.filter_by(name=special_category_name).first()
 
-# Flash Sale routes
-@app.route('/flash_sale', methods=['GET'])
-def handle_flash_sale():
-    flash_sales = FlashSale.query.all()
-    return jsonify([flashsale_serializer(sale) for sale in flash_sales])
+    if special_category and item:
+        item.special_categories.remove(special_category)
+        db.session.commit()
+        return jsonify({"message": f"Special Category {special_category_name} removed from item"}), 200
+    return jsonify({"message": "Error: Item or Special Category not found"}), 404
 
-# Hot In Category routes
-@app.route('/hot_in_category', methods=['GET'])
-def handle_hot_in_category():
-    hot_items = HotInCategory.query.all()
-    return jsonify([hotincategory_serializer(item) for item in hot_items])
 
-# Artwork routes
-@app.route('/artwork', methods=['GET'])
-def handle_artwork():
-    artworks = Artwork.query.all()
-    return jsonify([artwork_serializer(art) for art in artworks])
+# # Clothes routes
+# @app.route('/clothes', methods=['GET'])
+# def handle_clothes():
+#     clothes = Clothes.query.all()
+#     return jsonify([clothes_serializer(cloth) for cloth in clothes])
 
-# Shoes routes
-@app.route('/shoes', methods=['GET'])
-def handle_shoes():
-    shoes_list = Shoes.query.all()
-    return jsonify([shoes_serializer(shoe) for shoe in shoes_list])
+# # Whats New routes
+# @app.route('/whats_new', methods=['GET'])
+# def handle_whats_new():
+#     whats_new_items = WhatsNew.query.all()
+#     return jsonify([whatsnew_serializer(item) for item in whats_new_items])
 
-# Electronics routes
-@app.route('/electronics', methods=['GET'])
-def handle_electronics():
-    electronics_list = Electronics.query.all()
-    return jsonify([electronics_serializer(electronic) for electronic in electronics_list])
+# # Flash Sale routes
+# @app.route('/flash_sale', methods=['GET'])
+# def handle_flash_sale():
+#     flash_sales = FlashSale.query.all()
+#     return jsonify([flashsale_serializer(sale) for sale in flash_sales])
 
-# Books routes
-@app.route('/books', methods=['GET'])
-def handle_books():
-    books = Book.query.all()
-    return jsonify([book_serializer(book) for book in books])
+# # Hot In Category routes
+# @app.route('/hot_in_category', methods=['GET'])
+# def handle_hot_in_category():
+#     hot_items = HotInCategory.query.all()
+#     return jsonify([hotincategory_serializer(item) for item in hot_items])
 
-# Cart routes
-@app.route('/cart', methods=['GET'])
-def handle_cart():
-    carts = Cart.query.all()
-    return jsonify([cart_serializer(cart) for cart in carts])
+# # Artwork routes
+# @app.route('/artwork', methods=['GET'])
+# def handle_artwork():
+#     artworks = Artwork.query.all()
+#     return jsonify([artwork_serializer(art) for art in artworks])
 
-# Cart Item routes
-@app.route('/cart_item', methods=['GET'])
-def handle_cart_items():
-    cart_items = CartItem.query.all()
-    return jsonify([cartitem_serializer(item) for item in cart_items])
+# # Shoes routes
+# @app.route('/shoes', methods=['GET'])
+# def handle_shoes():
+#     shoes_list = Shoes.query.all()
+#     return jsonify([shoes_serializer(shoe) for shoe in shoes_list])
+
+# # Electronics routes
+# @app.route('/electronics', methods=['GET'])
+# def handle_electronics():
+#     electronics_list = Electronics.query.all()
+#     return jsonify([electronics_serializer(electronic) for electronic in electronics_list])
+
+# # Books routes
+# @app.route('/books', methods=['GET'])
+# def handle_books():
+#     books = Book.query.all()
+#     return jsonify([book_serializer(book) for book in books])
+
+# # Cart routes
+# @app.route('/cart', methods=['GET'])
+# def handle_cart():
+#     carts = Cart.query.all()
+#     return jsonify([cart_serializer(cart) for cart in carts])
+
+# # Cart Item routes
+# @app.route('/cart_item', methods=['GET'])
+# def handle_cart_items():
+#     cart_items = CartItem.query.all()
+#     return jsonify([cartitem_serializer(item) for item in cart_items])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
