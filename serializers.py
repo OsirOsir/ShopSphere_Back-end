a31e1863aecc1b95c1fcd584ec65d44a52cff850
shalyne23 <shalynelangat@gmail.com>
2024-10-16 12:50:38 +0300

    Add item file

--- serializers.py
+++ serializers.py
@@ -1,19 +1,45 @@
-def user_serializer(user):
-    return {
-        "id": user.id,
-        "name": user.name,
-        "email": user.email,
+# def user_serializer(user):
+#     return {
+#         "id": user.id,
+#         "name": user.name,
+#         "email": user.email,
        
+# # serializers.py
+
+# def item_serializer(item):
+#     return {
+#         "id": item.id,
+#         "name": item.name,
+#         "description": item.description,
+#         "price": item.price,
+#         "category": item.category,
+#         "items_available": item.items_available,
+#         "offer_price": item.offer_price,
+#         "image_url": item.image_url
+#     }
+
+
 # serializers.py
 
 def item_serializer(item):
     return {
-        "id": item.id,
-        "name": item.name,
-        "description": item.description,
-        "price": item.price,
-        "category": item.category,
-        "items_available": item.items_available,
-        "offer_price": item.offer_price,
-        "image_url": item.image_url
+        'id': item.id,
+        'item_name': item.item_name,
+        'description': item.description,
+        'price': item.price,
+        'category': item.category,
+        'product_quantity': item.product_quantity,
+        'image_url': item.image_url,
+        'is_in_stock': item.is_in_stock()  
+    }
+
+
+def notification_serializer(notification):
+    """Serialize a Notification object into a dictionary."""
+    return {
+        'id': notification.id,
+        'user_id': notification.user_id,
+        'item_id': notification.item_id,
+        'item_name': notification.item.item_name,  
+        'message': notification.message
     }
